ba8c9e772410a042c7ea0fa2de05e74672b7dcfb
simonwsesuraj <simonwsesuraj@gmail.com>
2025-09-24 10:02:57 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
 }
 
 # CORS (development: allow localhost Vite; in production set the actual origin)
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://student-form-frontend.onrender.com",
